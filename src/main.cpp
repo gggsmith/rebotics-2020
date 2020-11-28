6d81033c6cbdf0f9c7361d7ddff377bf7e6768dc
Liubov Ulitina <ulitinalm@vl.ru>
2020-11-28 19:07:46 +1000

    - Подключаю энкодер https://github.com/PaulStoffregen/Encoder - Подключаю серво-привод https://github.com/arduino-libraries

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <Arduino.h>
 #include <SparkFun_TB6612.h>
+#include <Encoder.h>
+#include <Servo.h>
 
 // Драйвер моторов
 #define AIN1 9
@@ -9,23 +11,44 @@
 #define PWMA 11
 #define PWMB 5
 #define STBY 8
-// these constants are used to allow you to make your motor configuration 
-// line up with function names like forward.  Value can be 1 or -1
 const int offsetA = 1;
 const int offsetB = 1;
 
-Motor motorA = Motor(AIN1, AIN2, PWMA, offsetA, STBY);
 Motor motorB = Motor(BIN1, BIN2, PWMB, offsetB, STBY);
+
+
+// Энкодер мотора
+// Для лучшей производительности должны быть подключены в interruptable pin (хотя бы a-фаза)
+#define APHASE 2
+#define BPHASE 3
+
+Encoder encoderB = Encoder(APHASE, BPHASE);
+
+
+
+// Серво-привод
+#define SERVO 4
+Servo servo;
+
 
 void setup() {
   Serial.begin(9600);
+  servo.attach(SERVO);
 }
 
 void loop() {
 
+  servo.write(0);
+  delay(1000);
+  servo.write(180);
+  delay(1000);
+
+
   motorB.drive(255,1000);
+  Serial.println(encoderB.read());
   motorB.drive(-255,1000);
+  Serial.println(encoderB.read());
   motorB.brake();
-  delay(10000);
+  delay(5000);
   
 }
